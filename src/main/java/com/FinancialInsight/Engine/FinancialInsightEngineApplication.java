e8c571a977d53ae97b4cbd4611bd404f963fee33
Oliver-Cairns <olivercawe@gmail.com>
2023-06-18 16:27:26 +0100

    Spring boot setup

---
+++ src/main/java/com/FinancialInsight/Engine/FinancialInsightEngineApplication.java
@@ -0,0 +1,13 @@
+package com.FinancialInsight.Engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialInsightEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialInsightEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FinancialInsight/Engine/FinancialInsightEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinancialInsight.Engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInsightEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
